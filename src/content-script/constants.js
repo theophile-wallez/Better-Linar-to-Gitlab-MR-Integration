7f44ffb7a05482c0e066cbc39beb4bba60168ab8
Théophile Wallez <theophile.wallez@sia-partners.com>
2025-01-16 14:23:39 +0100

    update regex

--- src/content-script/constants.js
+++ src/content-script/constants.js
@@ -1,4 +1,4 @@
-const API_URL = "https://api.linear.app/graphql";
-const ISSUE_NAME_REGEX = /[A-Za-z0-9]{1,5}-\d+/g;
+const API_URL = 'https://api.linear.app/graphql';
+const ISSUE_NAME_REGEX = /[A-Za-z0-9]{1,8}-\d+/g;
 const TITLE_SELECTOR = '[data-testid="title-content"]';
 const DESCRIPTION_SELECTOR = '[data-testid="description-content"]';
